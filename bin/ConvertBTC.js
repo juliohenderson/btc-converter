36bb60bcf3347cab6e2faddd28b735b0b5ad00d2
Júlio Henderson <julio.henderson98@gmail.com>
2019-10-02 22:03:04 -0300

    build

---
+++ bin/ConvertBTC.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var convertBTC = function convertBTC() {
+  var currency = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'USD';
+  var amount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return amount + ' BTC to ' + currency + ' = 2000.00';
+};
+
+module.exports = convertBTC;
