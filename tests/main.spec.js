099b1ac4a66cdb52381f4ef324cac7bf901a0cdf
Júlio Henderson <julio.henderson98@gmail.com>
2019-10-02 22:01:29 -0300

    feat(Main): add option for CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,42 @@
-import { expect } from 'chai';
+const { expect } = require('chai');
+const { exec } = require('child_process');
+const pkg = require('../package.json');
 
-describe('Main', () => {
+const btcConverter = 'node.exe ./src/main.js';
 
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, out, stderr) => {
+      const stdout = out;
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return currency option when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, out, stderr) => {
+      const stdout = out;
+      if (err) throw err;
+      expect(stdout.includes('--currency')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return amount option when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, out, stderr) => {
+      const stdout = out;
+      if (err) throw err;
+      expect(stdout.includes('--amount')).to.be.true;
+      done();
+    });
+  });
 });
